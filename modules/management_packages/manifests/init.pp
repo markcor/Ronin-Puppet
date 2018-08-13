eb5f55d22c6b495a0775cb7817f5b03cb8e29d15
markcor <mcornmesser@mozilla.com>
2018-08-13 13:23:49 -0700

    -

--- modules/management_packages/manifests/init.pp
+++ modules/management_packages/manifests/init.pp
@@ -1,7 +1,15 @@
 class management_packages {
 
+  include dirs
+
+  file { "C:\\packages\\nxlog-ce-2.9.1716.msi":
+    require => File["C:\\packages"],
+    ensure  => present,
+    source  => 'https://nxlog.co/system/files/products/files/348/nxlog-ce-2.9.1716.msi',
+  }
   package { 'nxlog':
-    ensure          => 'installed',
-    source          => 'https://nxlog.co/system/files/products/files/348/nxlog-ce-2.9.1716.msi',
+    ensure          => installed,
+    source          => "C:\\packages\\nxlog-ce-2.9.1716.msi",
+		
   }
 }
