b88eb072f7ae019e7d2d3929222efed05f0e0798
markcor <mcornmesser@mozilla.com>
2018-12-14 13:13:32 -0800

    ..

--- site/profiles/manifests/common_sys_config.pp
+++ site/profiles/manifests/common_sys_config.pp
@@ -2,7 +2,7 @@ class profiles::common_sys_config {
 
 	$disbaled_services = ["wsearch", "VSS", "puppet"]
 
-	class { 'disable_service':
+	class { 'services_disable_service':
 		disbaled_services => disbaled_services
 	}
 }
