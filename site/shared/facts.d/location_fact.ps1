3c6672ce29081e36fd626f6dbc4c5fdde3b4fc1e
markcor <mcornmesser@mozilla.com>
2018-12-18 15:46:07 -0800

    add location facts

---
+++ site/shared/facts.d/location_fact.ps1
@@ -0,0 +1,24 @@
+$DhcpDomain = ((Get-ItemProperty 'HKLM:SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters').'DhcpDomain')
+
+if ($DhcpDomain -like "*ec2*") {
+	$location = "aws"
+} else {
+	$location = "datacenter"
+}
+write-host $DhcpDomain
+write-host "$location"
+
+if ($location = "datacenter") {
+	if ($DhcpDomain -like "*MDC1*") {
+		$mozspace = "mdc1"
+	} elseif ($DhcpDomain -like "*MDC2*") {
+		$mozspace = "mdc2"
+	} elseif ($DhcpDomain -like "*MTV2*") {
+		$mozspace = "mtv2"
+	} else {
+		$mozspace = "unkown"
+	}
+}
+
+Write-host location=$location
+Write-host mozspace=$mozspace
