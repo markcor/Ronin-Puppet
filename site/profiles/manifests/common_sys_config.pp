be46789a235ff3246fda4d1b1a0c443962da9fe9
markcor <mcornmesser@mozilla.com>
2018-12-18 16:13:29 -0800

    corection

--- site/profiles/manifests/common_sys_config.pp
+++ site/profiles/manifests/common_sys_config.pp
@@ -2,4 +2,9 @@ class profiles::common_sys_config {
 
 	include services::disable_service
 	include win_defend::disable_windows_defender
+
+	file { "C:\\ronin\\test.txt" :
+		ensure  => present,
+		content => "$mozspace",
+	}
 }
