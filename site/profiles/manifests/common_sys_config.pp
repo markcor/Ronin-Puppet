9633ea555ac5d6228a9ae80ab64c9cbea00dc04b
markcor <mcornmesser@mozilla.com>
2018-12-19 08:30:08 -0800

    add w32time managment

--- site/profiles/manifests/common_sys_config.pp
+++ site/profiles/manifests/common_sys_config.pp
@@ -1,10 +1,31 @@
 class profiles::common_sys_config {
+	# Forge modules
+	include ::windowstime
 
+	# Custom modules
 	include services::disable_service
 	include win_defend::disable_windows_defender
 
 	file { "C:\\ronin\\test.txt" :
 		ensure  => present,
 		content => $facts['location'],
+	}
+
+	if $facts['location'] == 'aws' {
+		class { 'windowstime':
+ 			servers => { '0.pool.ntp.org'     => '0x01',
+						 '1.pool.ntp.org'     => '0x01',
+						 '2.pool.ntp.org'     => '0x01',
+						 '3.pool.ntp.org'     => '0x01',
+					  }	
+			timezone => 'Greenwich Standard Time',
+		}
+	}
+	if $facts['location'] == 'datacenter' {
+		class { 'windowstime':
+			servers  => { "infoblox1.private.$facts['mozspace'].mozilla.com" => '0x01',
+						}
+  			timezone => 'Greenwich Standard Time',
+		}
 	}
 }
