9a8809cf4b2f20513af9f9f80dc9c5091feb8587
markcor <mcornmesser@mozilla.com>
2018-12-19 15:57:07 -0800

    -

--- site/profiles/manifests/common_sys_config.pp
+++ site/profiles/manifests/common_sys_config.pp
@@ -1,11 +1,12 @@
 class profiles::common_sys_config {
 	# Forge modules
-	include ::windowstime
 
 	# Custom modules
 	include services::disable_service
+	include win_defend::disable_windows_defender
 	include win_update::disable_windows_update
 
+	# Place to test facts
 	file { "C:\\ronin\\test.txt" :
 		ensure  => present,
 		content => $facts['location'],
