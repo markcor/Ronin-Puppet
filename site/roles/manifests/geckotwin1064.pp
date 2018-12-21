7eaa70faf6fa3bbe0722b555cb397fe26cc798de
markcor <mcornmesser@mozilla.com>
2018-12-21 10:01:51 -0800

    add cloud and hw common profiles

--- site/roles/manifests/geckotwin1064.pp
+++ site/roles/manifests/geckotwin1064.pp
@@ -1,4 +1,5 @@
 class roles::geckotwin1064 {
 	# System Configuration
 	include profiles::common_sys_config
+	include profiles::cloud_common_sys_config
 }
---
+++ site/roles/manifests/geckotwin1064hw.pp
@@ -0,0 +1,4 @@
+class roles::geckotwin1064hw  {
+	# System Configuration
+	include profiles::common_sys_config
+}
