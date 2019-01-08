a8c7d0c1743cee7b823c6d956fe4503b2fefcd86
markcor <mcornmesser@mozilla.com>
2019-01-08 09:32:05 -0800

    >>>>

--- site/profiles/manifests/common_sys_config.pp
+++ site/profiles/manifests/common_sys_config.pp
@@ -7,7 +7,6 @@ class profiles::common_sys_config {
 	# Forge modules
 
 	# Custom modules
-	include win_services::disable_service
 	include win_defend::disable_windows_defender
 	include win_update::disable_windows_update
 
