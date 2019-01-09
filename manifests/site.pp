b99fe69865dfc47d20b58852e9d99f8f9fb22a55
markcor <mcornmesser@mozilla.com>
2019-01-08 16:13:26 -0800

    ::

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,8 @@
 node default {
 	include roles::geckotwin1064
 }
+
+# hardware
+node /^t-w1064.*\.(wintest|test)\.releng\.(mdc*)\.mozilla.com$ {
+	include hardware_common_sys_config
+}
---
+++ site/profiles/manifests/hardware_common_sys_config.pp
@@ -0,0 +1,18 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class profiles::hardware_common_sys_config {
+
+	class { 'windowstime':
+		servers  => { "infoblox1.private.$facts['mozspace'].mozilla.com"     => '0x08'}, 
+		timezone => 'Greenwich Standard Time',
+	}
+}
+
+# Bug list
+# Main
+# https://bugzilla.mozilla.org/show_bug.cgi?id=1515777
+
+# NTP 
+# https://bugzilla.mozilla.org/show_bug.cgi?id=1510754
