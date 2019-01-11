a2543d10a257045cfdfca35818520bdbc51c99fa
markcor <mcornmesser@mozilla.com>
2019-01-11 11:12:31 -0800

    ::

--- site/roles/manifests/geckotwin1064hw.pp
+++ site/roles/manifests/geckotwin1064hw.pp
@@ -1,5 +1,13 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/
+
 class roles::geckotwin1064hw  {
+
 	# System Configuration
 	include profiles::common_sys_config
 	include profiles::hardware_common_sys_config
+
+	# Admin Configuration
+	include profiles::common_admin
 }
