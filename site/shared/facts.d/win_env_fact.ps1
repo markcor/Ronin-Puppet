4664d2f5eda0e877b6099edbd7bebd5fceb72ebc
markcor <mcornmesser@mozilla.com>
2019-01-17 10:04:03 -0800

    :

--- site/shared/facts.d/win_env_fact.ps1
+++ site/shared/facts.d/win_env_fact.ps1
@@ -1,4 +1,4 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
+s Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
@@ -8,8 +8,15 @@ $programdata     = $env:programdata.replace("\","\\")
 $programfiles    = $env:ProgramW6432.replace("\","\\")
 $programfilesx86 = "$env:ProgramFiles(x86)".replace("\","\\")
 
-
+# Environment variables
 write-host "systemdrive=$env:systemdrive\\"
 write-host "programdata=$programdata\\"
 write-host "programfiles=$programfiles\\"
 write-host "programfilesx86=$programfilesx86\\"
+
+
+# Facts built off of environment variables
+write-host "roninprogramdata=$programdata\\PuppetLabs\\ronin"
+write-host "roninsemaphoredir=$programdata\\PuppetLabs\\ronin\\semaphore"
+write-host "tempdir=$env:systemdrive\\Windows\\Temp"
+
