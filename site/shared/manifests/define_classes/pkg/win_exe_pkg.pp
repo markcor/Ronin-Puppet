f4e1676397bd829f79fad59085cc3c58bed5c307
markcor <mcornmesser@mozilla.com>
2019-01-22 12:27:16 -0800

    <>

--- site/shared/manifests/define_classes/pkg/win_exe_pkg.pp
+++ site/shared/manifests/define_classes/pkg/win_exe_pkg.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-define shared::pkg::win_exe_pkg ( $pkg, $package=$title, $install_options_string=[]) {
+define shared::define_classes::pkg::win_exe_pkg ( $pkg, $package=$title, $install_options_string=[]) {
 
 include shared::dirs::win_ronin_dirs
 
