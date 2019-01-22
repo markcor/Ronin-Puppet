63bb7a958105142c24e565de4fbe7666dd424204
markcor <mcornmesser@mozilla.com>
2019-01-22 12:20:28 -0800

    ()

--- site/profiles/manifests/common_admin.pp
+++ site/profiles/manifests/common_admin.pp
@@ -15,40 +15,40 @@ class profiles::common_admin {
 		ensure => present,
 		source => "https://s3.amazonaws.com/windows-opencloudconfig-packages/RoninPackages/jq-win64.exe",
 	}
-	shared::pkg::win_msi_pkg  { "7-Zip 18.06 (x64 edition)":
+	shared::define_classes::pkg::win_msi_pkg  { "7-Zip 18.06 (x64 edition)":
 		pkg             => "7z1806-x64.msi",
 		install_options => ['/quiet'],
 	}
-	shared::pkg::win_zip_pkg { 'proc_expolorer':
+	shared::define_classes::pkg::win_zip_pkg { 'proc_expolorer':
 		pkg         => 'ProcessExplorer.zip',
 		creates     => "C:\\ProcessExplorer\\procexp.exe",
 		destination => "C:\\ProcessExplorer",
 	}
-	shared::pkg::win_zip_pkg { 'proc_monitor':
+	shared::define_classes::pkg::win_zip_pkg { 'proc_monitor':
 		pkg         => 'ProcessMonitor.zip',
 		creates     => "C:\\ProcessMonitor\\Procmon.exe",
 		destination => "C:\\ProcessMonitor",
 	}
-    shared::pkg::win_exe_pkg  { "gpg4win-2.3.0":
+    shared::define_classes::pkg::win_exe_pkg  { "gpg4win-2.3.0":
         pkg             => "gpg4win-2.3.0.exe",
         install_options_string => "/S",
     }
-    shared::pkg::win_exe_pkg  { "sublime_text":
+    shared::define_classes::pkg::win_exe_pkg  { "sublime_text":
         pkg             => "SublimeTextBuild3176x64Setup.exe",
         install_options_string => "/VERYSILENT /NORESTART /TASKS=\"contextentry\"",
     }
 
 	# Nxlog and configuration
-    shared::pkg::win_msi_pkg  { "NxLog-CE":
+    shared::define_classes::pkg::win_msi_pkg  { "NxLog-CE":
         pkg             => "nxlog-ce-2.8.1248.msi",
         install_options => ['/quiet'],
     }
 	file { "$programfilesx86\\nxlog\\cert\\papertrail-bundle.pem":
 		source => "$site_source/profiles/files/common_admin/papertrail-bundle.pem",
 	}
-    file { "$programfilesx86\\nxlog\\conf\\nxlog.conf":
-        source => epp("$site_source/profiles/templates/common_admin/nxlog.conf.epp"),
-    }
+   # file { "$programfilesx86\\nxlog\\conf\\nxlog.conf":
+   #     source => epp("$site_source/profiles/templates/common_admin/nxlog.conf.epp"),
+   # }
 }	
 
 # Bug list
