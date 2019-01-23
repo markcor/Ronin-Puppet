423d4d41a6f63ce098d118332a6ce517d6517e55
markcor <mcornmesser@mozilla.com>
2019-01-23 15:23:09 -0800

    <>

--- site/defined_classes/manifests/srv/disable_service.pp
+++ site/defined_classes/manifests/srv/disable_service.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-define defined_classes::srv ::disable_service ($service=$title) {
+define defined_classes::srv::disable_service ($service=$title) {
 
 	service { $service:
 		ensure => 'stopped',
