cc919e02e1dcca0be8e3ec1ac12c2518d28b9f29
markcor <mcornmesser@mozilla.com>
2019-02-01 08:58:23 -0800

    <>

--- site/profiles/manifests/common_admin.pp
+++ site/profiles/manifests/common_admin.pp
@@ -10,7 +10,7 @@ class profiles::common_admin {
 
 	include win_packages::jq
 	include win_packages::gpg4win
-#	include win_packages::nxlog
+	include win_packages::nxlog
 	include win_packages::process_debug
 	include win_packages::sevenzip
 	include win_packages::sublimetxt
