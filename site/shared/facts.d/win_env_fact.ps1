d4dd39e4f854ad6417a1c5571aced0c60b913032
markcor <mcornmesser@mozilla.com>
2019-02-04 16:00:17 -0800

    add system32 fact

--- site/shared/facts.d/win_env_fact.ps1
+++ site/shared/facts.d/win_env_fact.ps1
@@ -1,16 +1,18 @@
-# Source Code Form is subject to the terms of the Mozilla Public
+ource Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # Creates useful facts based off of system environment variables
 
 $systemdrive     = $env:systemdrive
-$programdata     = $env:programdata
-$programfiles    = $env:ProgramW6432
+$system32        = $env:systemdrive\\system32
+$programdata     = $env:programdata.replace("\","\\")
+$programfiles    = $env:ProgramW6432.replace("\","\\")
 $programfilesx86 = "$systemdrive\\Program Files (x86)"
 
 # Environment variables
 write-host "systemdrive=$env:systemdrive"
+write-host "system32=$system32"
 write-host "programdata=$programdata"
 write-host "programfiles=$programfiles"
 write-host "programfilesx86=$programfilesx86"
